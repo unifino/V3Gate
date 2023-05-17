b7a838d5af1737578a2d968547c5bfe3995e8ea4
unifino <unifino@protonmail.com>
2023-05-17 21:19:46 +0330

    ...

--- src/server.ts
+++ src/server.ts
@@ -374,7 +374,7 @@ async function groupName ( db: SQL_lite_3.Database, container: TS.Users ): Promi
 
         tmpName = rows[i].remark.split( 'PPS' )[0].trim();
 
-        let RSX = [ "Rasul X08", "Rasul X09", "Rasul X10", "Hatef" ];
+        let RSX = [ "Rasul X08", "Rasul X09", "Rasul X10" ];
         if ( RSX.includes( tmpName ) ) tmpName = "RSX";
 
         // .. create new user
